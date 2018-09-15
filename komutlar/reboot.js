6f5f984274439d24a2ea1ec023fc661ed84a913f
Vuqar717ka <42815314+Vuqar717ka@users.noreply.github.com>
2018-09-15 21:55:51 +0400

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -1,21 +1,16 @@
 const Discord = require('discord.js');
 
 
-exports.run = function(client, message) { 
-}
-  
-  
-  if (message.author.id === "342380541003300865") {
-    message.channel.send('myBot yeniden başlatılıyor...').then(msg => {
-      console.log('myBot yeniden başlatılıyor.')
-      process.exit(0);
+exports.run = function(client, message) {
+
+if (message.author.id === "342380541003300865")  {
+  message.channel.send("Bot yeniden başlatılıyor").then(msg => {
+        console.log("[BOT]Yeniden başlatılıyor");
+        process.exit(0);
     });
-  } else 
+ } else 
     message.channel.send('Maalesef bu komutu kullanamazsın. Benim yapımcım değilsin!')
-
-      
-
-;
+};
 
 exports.conf = {
   enabled: true, 
