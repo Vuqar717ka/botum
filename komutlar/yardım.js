6e430414e73ed09f3259e82b16cf54e5eec36647
Vuqar717ka <42815314+Vuqar717ka@users.noreply.github.com>
2018-09-20 22:45:14 +0400

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -8,10 +8,10 @@ exports.run = (client, message) => {
   .setColor('RANDOM')
   .setFooter('DailyBot\'s', bot.user.avatarURL)
   .addField("**❯Komutlar)"
-  .addField("»v!1vs1"             :: Seçtiğiniz 2 kişiyi savaştırırsınız.)
-  .addField("»v!8ball"            :: Sihirli 8 ball sorularınızı cevaplar.)
-  .addField("»v!coolresim"        :: Coolresim gösterir.)
-  .addField("»v!herkesebendençay" :: Herkese Çay Verir.)
+  .addField("»v!1vs1"             :: Seçtiğiniz 2 kişiyi savaştırırsınız, true)
+  .addField("»v!8ball"            :: Sihirli 8 ball sorularınızı cevaplar, true)
+  .addField("»v!coolresim"        :: Coolresim gösterir, true)
+  .addField("»v!herkesebendençay" :: Herkese Çay Verir, true)
 
 exports.conf = {
   enabled: true,
